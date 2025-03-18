0c0aad5c525ade9eec3416e0171055ee78e5458b
Akatronix <153951875+Akatronix@users.noreply.github.com>
2025-03-18 17:59:00 +0100

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -113,26 +113,7 @@ const LoginPage = () => {
           >
             {isLoading ? (
               <>
-                <svg
-                  className="mr-3 size-5 animate-spin"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  />
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 018 8h-4l3 3 3-3h-4a8 8 0 01-8 8v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
-                  />
-                </svg>
+             
                 Loading...
               </>
             ) : (
